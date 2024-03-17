2fab445100448225aa3b63fc9e5fbf7828696a91
Nile Jocson <novoseiversia@gmail.com>
2024-03-18 07:48:19 +0800

    Test library in main

--- executable/src/main.cxx
+++ executable/src/main.cxx
@@ -1,11 +1,13 @@
 #include <cstdlib>
 
 #include "greet.hxx"
+#include "xxinit/greet.hxx"
 
 
 
 auto main() -> int
 {
 	novoseiversia::xxinit::greet("novoseiversia");
+	novoseiversia::xxinit::greet(novoseiversia::xxinit::Language::Filipino, "novoseiversia");
 	return EXIT_SUCCESS;
 }
